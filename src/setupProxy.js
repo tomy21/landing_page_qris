ab1489f6749dbbb5e1037f45e03a954fbe174bed
DESKTOP-MIFG50S\Lenovo <tomy21.agung@gmail.com>
2024-01-27 13:16:18 +0700

    updated

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/general/Partner/InquiryTransaction", {
-      target: "http://147.139.135.195:2101",
+      target: "https://api-testing-murex.vercel.app",
       changeOrigin: true,
     })
   );
